799306643ea7f9b7f616083a3d5069b4a6f1aa38
Groon1k <45107027+Groon1k@users.noreply.github.com>
2020-11-22 20:53:28 +0300

    Add files via upload

---
+++ sniffer/sniffer_22.py
@@ -0,0 +1,9 @@
+import scapy.all as scapy
+
+def sniff(interface):
+    scapy.sniff(iface = interface, store = False, prn = process_sniffed_packet, filter = 'port 22')
+
+def process_sniffed_packet(packet):
+    print(packet.summary())
+
+sniff("eth0")
